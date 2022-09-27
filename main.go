b6baf658fbe930be363143ef9e0035a9522404bb
charlotte-z <2698028166@qq.com>
2022-09-27 15:20:24 +0800

    test dom in golang

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
+
+
 package main
 
-import "fmt"
+import (
+	"syscall/js"
+)
+
+func hello(this js.Value, args []js.Value) interface{} {
+	doc := js.Global().Get("document")
+
+	// title = document.createElement("h1")
+	// title.innerText = "Hello World"
+	title := doc.Call("createElement", "h1")
+	title.Set("innerText", "Hello World")
+
+	// document.body.append(title)
+	doc.Get("body").Call("append", title)
+	return nil
+}
 
 func main() {
-	fmt.Println("Hello World from Golang")
+	js.Global().Set("hello", js.FuncOf(hello))
+	select {} // block the main thread forever
 }
